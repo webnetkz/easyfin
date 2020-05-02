ddf7d4368807d0cd621f04fecfac31e13b385f52
webnetkz <8gg001@gmail.com>
2020-05-02 15:43:08 +0600

    new update

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
-
-    require_once 'app/DB/PDO.php';
-
+    require_once 'mycompany.php';
 ?>
 <style>
     body {
         font-family: sans-serif;
+        transition-duration: 900ms;
     }
     .inp {
-        display: block;
         margin: 10px;
         width: 20em;
         text-align: center;
@@ -23,36 +21,57 @@
     }
 </style>
 
-<h1>Счет на оплату</h1>
-<form action="#" method="POST">
-    <label for="contr">Действующий</label>
-    <input type="radio" class="inpR" name="cotnr[]" id="contr" onclick="oldContr();">
-    <label for="contrNew">Новый</label>
-    <input type="radio" class="inpR" name="cotnr[]" id="contrNew" onclick="newContr();">
-    <input type="text" class="inp" placeholder="Наименование контрагента" >
+<h1>Счет на оплату № <?=$order;?></h1>
+<form action="order.php" method="POST">
+    <h3>Выбор контрагента</h3>
+    <label for="contrNew" class="headerContr">Новый контрагент</label>
+    <input type="checkbox" class="inpR" name="contrNew" id="contrNew" oninput="appendContr();" value="1">
+        <hr>
+    <label for="nameContr">Выбрать контрагента</label>
+        <br>
+    <select name="nameContr" id="nameContr" class="inp">
+        <option value="Topol">Тополь</option>
+    </select>
+        <br>
+    <input type="text" name="newNameContr" placeholder="Наименование контрагента" class="inp newNameContr" style="display: none;">
 
     <div class="contrNew">
-        <hr>
-        <input type="text" class="inp" placeholder="БИН/ИИН">
-        <input type="text" class="inp" placeholder="Наименование">
-        <input type="text" class="inp" placeholder="Адрес">
-        <input type="text" class="inp" placeholder="БАНК"> 
+        <input type="text" class="inp" placeholder="БИН/ИИН" name="binContr">
+            <br>
+        <input type="text" class="inp" placeholder="Адрес" name="adresContr">
+            <br>
+        <input type="text" class="inp" placeholder="БАНК" name="bankContr"> 
         <hr> 
     </div>
 
-    <input type="text" class="inp" placeholder="Товар/Услуга">
-
-    <input type="text" class="inp">
+    <label for="nameService">Наименование товара</label>
+    <div class="nameService">
+            <br>
+        <select name="service" class="inp">
+            <option value="r">Ручка</option>
+            <option value="k">Карандаш</option>
+            <option value="b">Бумага</option>
+        </select>
+        <input type="text" placeholder="Колличиство" name="number">
+            <br>
+        <div id="appendServiceNew"></div>
+    </div>
+    <button>Выставить счет на опалту</button>
 </form>
 
 <script>
 
-    var contrNew = document.querySelector(".contrNew");
-    function newContr() {
-        contrNew.style.display = 'block';
+    var contrNew = document.querySelector("#contrNew");
+    var newContr = document.querySelector(".contrNew");
+    var headerContr = document.querySelector(".headerContr");
+    var nameContr = document.querySelector("#nameContr");
+    var newNameContr = document.querySelector(".newNameContr");
+    function appendContr() {
+            newNameContr.style.display = 'block';
+            newContr.style.display = 'block'
+            contrNew.style.display = 'none';
+            headerContr.style.display = 'none';
+            nameContr.style.display = 'none';
     }
-    function oldContr() {
-        contrNew.style.display = 'none';
-    }
-    console.log(contrNew);
+
 </script>
---
+++ order.php
@@ -0,0 +1,53 @@
+﻿<?php
+require_once 'mycompany.php';
+
+if( !empty($_POST) ) {
+
+    $nameContr = $_POST['nameContr'];
+    $service = $_POST['service'];
+    $number = $_POST['number'];
+
+    if($service == 'r') {
+        $service = 'Ручка';
+        $price = 120;
+    }
+    if($service == 'k') {
+        $service = 'Карандаш';
+        $price = 220;
+    }
+    if($service == 'b') {
+        $service = 'Бумага';
+        $price = 320;
+    }
+    $price = $price * $number;
+
+    if($_POST['newNameContr']) {
+        $newNameContr = $_POST['newNameContr'];
+        $binContr = $_POST['binContr'];
+        $adresContr = $_POST['adresContr'];
+        $bankContr = $_POST['bankContr'];
+    }
+    
+    echo '<pre>';
+}
+?>
+<h3>Счет на оплату № <?=$order;?> от <?=$date;?></h3>
+    <hr>
+<h2>Поставщик:</h2>
+    <p>Товарищество с ограниченной ответственностью <?=$name;?></p>
+    <p>БИН <?=$bin;?></p>
+    <p>КБЕ <?=$kbe;?></p>
+    <p>ИИК <?=$iik;?></p>
+    <p>Банк <?=$bank;?></p>
+    <p>Адрес <?=$adres;?></p>
+        <hr>
+<h2>Покупатель:</h2>
+    <p>Товарищество с ограниченной ответственностью <?=$client_name;?></p>
+    <p>БИН <?=$client_bin;?></p>
+    <p>Адрес <?=$client_adres;?></p>
+        <hr>
+<h2>Товары\Услуги:</h2>
+    <p>Наименование: <?=$service;?></p>
+    <p>Колличество: <?=$number;?></p>
+    <p>Общая стоимость: <?=$price;?></p>
+        <hr>
