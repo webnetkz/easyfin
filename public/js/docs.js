bc155facbf74faf064b13a29e32a9d827b50a5ea
webnetkz <8gg001@gmail.com>
2020-05-23 20:47:26 +0600

    append links for doc tables

--- public/js/docs.js
+++ public/js/docs.js
@@ -1,5 +1,30 @@
 ﻿let docs = document.querySelector('#docs');
 
+function getAllDocsLink() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'http://easyfin/app/querys/getAllDocsLinks.php');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+
+            let resultQueryX = xhr.responseText;
+            // Разбить строку, разделителем ',' на элементы массива
+            resultQueryX = resultQueryX.split(',');
+            resultQueryX.pop();
+
+            let docItemLink = document.querySelectorAll('.docItemLink');
+            
+            console.log(resultQueryX);
+            for(let i = 1; i < resultQueryX.length && i != resultQueryX.length -1; i++) {
+                docItemLink[i].Attribute('href',$resultQueryX[i]);
+            }
+        }
+    }
+    xhr.send();
+}
+
 docs.addEventListener('click', () => {
     let invoice = document.querySelector('#invoice');
     invoice.classList.remove('menuItemActive');
@@ -17,13 +42,14 @@ docs.addEventListener('click', () => {
         xhr.onreadystatechange = function() {
             if(xhr.readyState === 4 && xhr.status === 200) {
 
+                getAllDocsLink();
+
                 let resultQuery = xhr.responseText;
                 // Разбить строку, разделителем ',' на элементы массива
                 resultQuery = resultQuery.split(',');
                 component.innerHTML += '<p class="docItem" style="border-radius: 5px; background: rgb(77, 77, 77);"><span class="docTableItem">Номер</span><span class="docTableItem">Дата</span><span class="docTableItem">Контрагент</span></p>';
                 for(let i = 0; i < resultQuery.length && i != resultQuery.length -1; i++) {
-                    component.innerHTML += '<p class="docItem">'+resultQuery[i]+'</p>';
-   
+                    component.innerHTML += '<a class="docItemLink"><p class="docItem">'+resultQuery[i]+'</p></a>';
                 }
             }
         }
